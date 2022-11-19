a4aab4f0dd4d645ec6e38a73b4d361be8e05c789
bigsky77 <simonjudd2@gmail.com>
2022-11-19 19:57:21 +0200

    feat: build fix

--- components/header.tsx
+++ components/header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
             <ul class="flex flex-col p-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
                <li>
 
-                <Link href="/dapp" class="text-gray-900 bg-white hover:border-burned-gold border animate-pulse-slow border-magentaVibrant focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700">Launch dApp</Link>
+                <Link href="" class="text-gray-900 bg-white hover:border-burned-gold border animate-pulse-slow border-magentaVibrant focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700">Launch dApp</Link>
               </li>
             </ul>
           </div>
