0c6611dee4d390df6f2d1f77d50af09e58b8735d
bigsky77 <simonjudd2@gmail.com>
2022-11-23 20:29:00 +0200

    fix: updated register player

--- app/game/Play.tsx
+++ app/game/Play.tsx
@@ -10,6 +10,8 @@ const { abi } = require('abi/bigsky.json');
 const { useChainId, useAccounts, useIsActivating, useIsActive, useProvider, useENSNames} = hooks
 
 const Play = (props) => {
+  const [register, updateRegister] = useState(false);
+
   const provider = useProvider();
   const { library } = useWeb3React<Web3Provider>();
 
@@ -18,6 +20,10 @@ const Play = (props) => {
     bigsky.startGame();
   }
 
+  async function RegisterPlayer(props: address) {
+    bigsky.registerPlayer(address);
+  }
+  
   async function nextTurn() {
     const x = props.turn;
 
@@ -34,6 +40,13 @@ const Play = (props) => {
 
   return(
     <div class="flex space-x-4">
+    { register ? (
+      <div class="flex">
+        <button onClick={registerPlayer()}>Register Ship</button>
+        <input placeholder="0x123..."/>
+      </div>
+    ) : (
+    <div>
       <button onClick={StartGame} class="text-gray-900 bg-white hover:border-burned-gold focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg font-neue font-2xl px-2.5 py-1.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700"> 
         Play
       </button>
@@ -44,6 +57,8 @@ const Play = (props) => {
         Reset
       </button>
     </div>
+  )}
+  </div>
   )
 }
 
--- components/sprites/player.tsx
+++ components/sprites/player.tsx
@@ -3,11 +3,10 @@ const Player = (props: ship) => {
   return(
     <div>
         <p class="absolute font-neue text-3xl text-magentaVibrant"
-            style = {{top: props.ship[0] * 40 + 'px', left: props.ship[1] * 40 + 'px'}}
+            style = {{top: props.ship[0] * 40 + 100 + 'px', left: props.ship[1] * 40 + 'px'}}
          >
           🚀 
          </p>
-
     </div>
   )
 }
