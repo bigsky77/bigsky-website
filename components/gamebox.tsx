8a40c65bb98f8e99b88c6f8bd3d4eaa50ddc2025
bigsky77 <simonjudd2@gmail.com>
2022-11-23 10:28:40 +0200

    feat: fixed placement

--- components/gamebox.tsx
+++ components/gamebox.tsx
@@ -6,9 +6,13 @@ import Stars from './sprites/stars'
 const GameBox = ({stars, enemies, ship}: props) => {
   return(
     <div>
-      <Player ship={ship} />
-      <Enemies enemies={enemies} />
-      <Stars stars={stars} /> 
+      <div class="absolute left-24">
+        <div class="absolute top-32 left-96 z-10">
+          <Player ship={ship} />
+          <Enemies enemies={enemies} />
+          <Stars stars={stars} /> 
+        </div>
+      </div>
       <Grid /> 
     </div>
   )
