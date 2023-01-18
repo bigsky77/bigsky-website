9d59a1b0b543520687c6082573f837f95cf78956
Simon <simonjudd2@gmail.com>
2023-01-18 08:19:10 -0700

    fix: updated website address

--- address.ts
+++ address.ts
@@ -1,4 +1,4 @@
 import { transactions } from '../bigsky-contracts/broadcast/DeployBigSky.s.sol/31337/run-latest.json';
 
-export const address='0xf4Bf12135BFc954eb46C9c224f2E332eD523A754';//transactions[0].contractAddress;
+export const address='0x30a6fd4523b1cba2dda0f0a81a400441a9870eb3';//transactions[0].contractAddress;
 
