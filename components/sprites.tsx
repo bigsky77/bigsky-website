f26c2050ca49ca2ca1f17ae83b1463d20f43e7f7
Simon <simonjudd2@gmail.com>
2023-01-24 15:57:02 -0700

    fix:

--- components/sprites.tsx
+++ components/sprites.tsx
@@ -1,5 +1,7 @@
 import { Flipper, Flipped } from 'react-flip-toolkit'
 import React, { useState } from 'react'
+import Spaceship from '../public/spaceship.png' 
+import Image from 'next/image'
 
 const Sprites = ({ship, stars, turn, enemies }: props) => {
 
@@ -31,7 +33,7 @@ const Sprites = ({ship, stars, turn, enemies }: props) => {
           <Flipped flipId="ship">
             <p class="absolute font-neue text-3xl text-magentaVibrant"
              style = {{top: ship.positionY * 44.5 + 'px', left: ship.positionX * 44.5 + 'px'}}>
-               🚀
+              <Image src={Spaceship} class="w-24 pl-2" /> 
             <p class="text-sm">({ship.positionX},{ship.positionY})</p></p>
           </Flipped>
         )
