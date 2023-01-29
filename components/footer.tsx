b01dc6c7f89297b445a5b853effade9a9f58d2a1
Simon <simonjudd2@gmail.com>
2023-01-29 11:56:47 -0700

    fix: test

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return(
       <div>
       <footer class="fixed bottom-0 left-0 z-20 p-4 w-full bg-gradient-to-r from-purple-700 to-purple900 border-t-2 border-burned-gold shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-burned-gold">
-          <span class="text-sm text-soft-white sm:text-center dark:text-gray-400">©BigSky 2022 <a href="https://flowbite.com/" class="hover:underline"></a>
+          <span class="text-sm text-soft-white sm:text-center dark:text-gray-400">©BigSky 2023 <a href="https://flowbite.com/" class="hover:underline"></a>
           </span>
           <ul class="flex flex-wrap items-center mt-3 text-sm text-soft-white dark:text-gray-400 sm:mt-0">
               <li>
