0c59c800905346da70d9f772577140a7774cd8dd
Simon <simonjudd2@gmail.com>
2023-05-01 08:54:12 -0600

    reformat

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,14 @@ export default function Home() {
   return (
     <div class="h-screen bg-dark-cyan">
         <div class="flex justify-center">
-          <div class="absolute md:top-48 sm:top-48">
+          <div class="absolute md:top-64 top-64">
             <h1 class="text-center font-neue text-6xl md:text-9xl text-magentaVibrant">BigSky</h1>
             <br></br>
             <br></br>
             <h1 class="text-center font-neue text-6xl md:text-9xl text-magentaVibrant">🤖</h1>
-              <div class="font-italics pt-8 text-3xl md:text-6xl text-goldVibrant">
+              <div class="font-italics pt-8 text-2xl md:text-6xl text-goldVibrant">
                <Typewriter options={{skipAddStyles: true}} onInit={(typewriter) => {
-                  typewriter.typeString('we fucking love money...')
+                  typewriter.typeString('we fucking love crypto...')
                   .callFunction(() => {
                     console.log('String typed out!');
                   })
