3175b9fea3ebb87cb66465eae57f4ec0f35d8ef4
Simon <simonjudd2@gmail.com>
2023-06-19 12:32:47 -0600

    update tag line

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,20 +7,29 @@ import Footer from '../components/footer'
 import Sphynx from '../public/sphynx.jpg'
 import Photo35 from '../public/photo35.jpg'
 import Typewriter from 'typewriter-effect';
-import death_machine from '../public/death_machine.png'
 
 export default function Home() {
   return (
     <div class="h-screen bg-dark-cyan">
-      <Header/>
         <div class="flex justify-center">
-          <div class="absolute md:top-80 top-64">
-            <Link href="https://docs.google.com/forms/d/e/1FAIpQLSeK8NzmvEhpMRwhFWaUfWx7Lu_pqAPQcpnzFc8te4ZBA6-fYg/viewform?usp=sf_link">
-            <Image src={death_machine}/>
-            </Link>
+          <div class="absolute md:top-64 top-64">
+            <h1 class="text-center font-neue text-6xl md:text-9xl text-magentaVibrant">BigSky</h1>
+            <br></br>
+            <br></br>
+            <h1 class="text-center font-neue text-6xl md:text-9xl text-magentaVibrant">🤖</h1>
+              <div class="font-italics pt-8 text-2xl md:text-6xl text-goldVibrant">
+               <Typewriter options={{skipAddStyles: true}} onInit={(typewriter) => {
+                  typewriter.typeString('Attention Is All You Need!')
+                  .callFunction(() => {
+                    console.log('String typed out!');
+                  })
+                  .pauseFor(2500)
+                  .start();
+               }}
+            />
+           </div>
           </div>
         </div>
-      <Footer/>
     </div>
   )
 }
